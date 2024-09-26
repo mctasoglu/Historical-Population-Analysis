0ea93b2c6e37f137b6bf45a6eeedde7c23ef16f5
mctasoglu <mctasoglu@gmail.com>
2024-09-26 16:01:01 -0400

    Changes 26SEP2024

--- population.py
+++ population.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 def get_global_pop_at_year(df, yr):
@@ -132,6 +134,134 @@ def pop_turkey(main_df):
 
 
 
+def create_x_y(df_pop, df_gdp, df_poli_idx, df_military_spending):
+    
+    
+    x_y_dataset = pd.DataFrame()
+    
+    
+    
+    years = df_pop[ (df_pop['Year'] >= 1820) & (df_pop['Year'] < 1900)]['Year']
+   
+    
+    france_pop = df_pop[ (df_pop['Year'] >= 1820) & (df_pop['Year'] < 1900)]['France']
+    france_pop = france_pop.map(lambda x : x*1000)
+    france_gdp = df_gdp[ (df_gdp['Entity'] == 'France') & (df_gdp['Year'] >= 1820) & (df_gdp['Year'] < 1900) ]['Gross domestic product (GDP)']
+ 
+    france_poli_idx = df_poli_idx[ (df_poli_idx['Entity'] == 'France') & (df_poli_idx['Year'] >= 1820) & (df_poli_idx['Year'] < 1900) ]['Liberal component index (best estimate)']
+    france_military_spending = df_military_spending[ (df_military_spending['Entity'] == 'France') & (df_military_spending['Year'] >= 1820) & (df_military_spending['Year'] < 1900) ][r"Military expenditure (% of GDP)"]
+    print(france_gdp)
+    
+    x_y_dataset['France Population'] = france_pop
+    x_y_dataset.set_index(years, inplace=True)
+    
+    france_gdp.index = x_y_dataset.index
+    france_poli_idx.index = x_y_dataset.index
+    france_military_spending.index = x_y_dataset.index
+    
+    x_y_dataset['France GDP'] = france_gdp
+    
+    x_y_dataset[r"France Military Spending (% of GDP)"] = france_military_spending
+    x_y_dataset['France Political Index'] = france_poli_idx
+    
+    
+    # plt.scatter(x_y_dataset['France Population'], x_y_dataset['France GDP'], marker='x', color='blue')
+    # plt.xlabel('Population')
+    # plt.ylabel('GDP')
+    # plt.title('Population vs GDP in 19th Century France')
+    
+    # plt.show()
+    
+    
+    
+    
+    
+    
+    
+    return x_y_dataset
+
+
+def linear_analysis(dataset):
+    
+    
+    
+    X_train = dataset.loc[1820:1859, "France GDP" : r'France Military Spending (% of GDP)']
+    X_test = dataset.loc[1860:1899:, "France GDP" : r'France Military Spending (% of GDP)']
+    
+    y_train = dataset.loc[1820:1859, 'France Political Index']
+    y_test = dataset.loc[1860:1899, 'France Political Index']
+    
+    regr = linear_model.LinearRegression()
+    
+    regr.fit(X_train, y_train)
+    
+    y_pred = regr.predict(X_test)
+    
+    # The coefficients
+    print("Coefficients: \n", regr.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+    # The coefficient of determination: 1 is perfect prediction
+    print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+    
+    fig, axs = plt.subplots(1,2, sharey=True)
+    
+    cols = X_train.columns
+    print(dataset.tail(n=10))
+    
+    for i in range(len(cols)):
+        axs[i].scatter(X_train[cols[i]],y_train, label = 'target')
+        axs[i].set_xlabel(cols[i])
+        axs[i].scatter(X_train[cols[i]],y_pred,color="orange", label = 'predict')
+    
+    axs[0].set_ylabel("Political Index"); axs[0].legend()
+    
+    plt.show()
+    # Plot outputs
+    # plt.scatter(X_test, y_test, color="black")
+    # plt.plot(X_test, y_pred, color="blue", linewidth=3)
+
+    # plt.xticks(())
+    # plt.yticks(())
+
+    
+    
+    
+    return 0
+
+
+def open_datasheets():
+    
+        
+    maddison_pop = pd.read_excel(r"C:\Users\tasoglum\Desktop\Kaggle\mpd2023_web.xlsx", sheet_name='Population')
+    
+    maddison_gdp = pd.read_csv(r"C:\Users\tasoglum\Desktop\Kaggle\gdp-maddison-project-database.csv")
+    
+    poli_idx = pd.read_csv(r"C:\Users\tasoglum\Desktop\Kaggle\liberal-political-institutions-index.csv")
+    
+    military_spending = pd.read_csv(r"C:\Users\tasoglum\Desktop\Kaggle\military-spending-as-a-share-of-gdp-gmsd.csv")
+    
+    
+    
+    og_headers = maddison_pop.columns
+    
+    maddison_pop.drop([0,1], inplace=True)
+    maddison_pop.columns = og_headers
+    
+    maddison_pop.rename({"Population": "Year"}, axis=1, inplace=True)
+    maddison_pop.reset_index(inplace=True)
+    
+    france_datasheet = create_x_y(maddison_pop, maddison_gdp, poli_idx, military_spending)
+    
+    linear_analysis(france_datasheet)
+    
+    
+    
+    return 0
+
+
+
+
 def main():
     
     main_df = pd.read_csv(r"C:\Users\tasoglum\Desktop\Kaggle\population.csv")
@@ -143,26 +273,28 @@ def main():
     
     
     
-    pop_series = make_global_pop_series(main_df)
+    open_datasheets()
+    
+    # pop_series = make_global_pop_series(main_df)
     
  
-    ottoman_countries = ['Bosnia and Herzegovina', 'Turkey', 'Albania', 'Bulgaria', 'North Macedonia', 'Greece', 'Romania', 
-                         'Hungary', 'Moldova','Montenegro', 'Syria', 'Iraq', 'Kuwait', 'Lebanon', 'Israel', 'Egypt', 'Libya', 
-                         'Tunisia', 'Algeria', 'Ukraine', 'Yemen', 'Saudi Arabia', 'Cyprus']
+    # ottoman_countries = ['Bosnia and Herzegovina', 'Turkey', 'Albania', 'Bulgaria', 'North Macedonia', 'Greece', 'Romania', 
+    #                      'Hungary', 'Moldova','Montenegro', 'Syria', 'Iraq', 'Kuwait', 'Lebanon', 'Israel', 'Egypt', 'Libya', 
+    #                      'Tunisia', 'Algeria', 'Ukraine', 'Yemen', 'Saudi Arabia', 'Cyprus']
     
-    roman_countries = ['Bosnia and Herzegovina', 'Croatia', 'Slovenia', 'Turkey', 'Albania', 'Bulgaria', 'North Macedonia', 
-                        'Greece', 'Romania', 'Hungary', 'Montenegro', 'Syria', 'Lebanon', 'Israel', 'Egypt', 'Libya', 'Tunisia', 
-                        'Algeria', 'Morocco', 'Italy', 'Spain', 'Portugal', 'France', 'Belgium', 'United Kingdom', 'Austria',
-                        'Switzerland', 'Cyprus']
+    # roman_countries = ['Bosnia and Herzegovina', 'Croatia', 'Slovenia', 'Turkey', 'Albania', 'Bulgaria', 'North Macedonia', 
+    #                     'Greece', 'Romania', 'Hungary', 'Montenegro', 'Syria', 'Lebanon', 'Israel', 'Egypt', 'Libya', 'Tunisia', 
+    #                     'Algeria', 'Morocco', 'Italy', 'Spain', 'Portugal', 'France', 'Belgium', 'United Kingdom', 'Austria',
+    #                     'Switzerland', 'Cyprus']
     
     
     
-    non_ottoman_europe = ['France', 'Spain', 'Portugal', 'United Kingdom', 'Germany', 'Poland', 'Austria', 'Italy', 'Czechia', 
-                          'Slovakia', 'Belgium', 'Netherlands', 'Russia']
+    # non_ottoman_europe = ['France', 'Spain', 'Portugal', 'United Kingdom', 'Germany', 'Poland', 'Austria', 'Italy', 'Czechia', 
+    #                       'Slovakia', 'Belgium', 'Netherlands', 'Russia']
     
    
     
-    pop_ottoman_empire(main_df, ottoman_countries)
+    # pop_ottoman_empire(main_df, ottoman_countries)
     
     # turk_df = pop_turkey(main_df)
     
